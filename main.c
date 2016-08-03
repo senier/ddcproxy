e5e69ae858e3c885220ecb5d11a20eeb6bc7eb7e
danielmatusek <da.matusek@gmail.com>
2016-08-03 13:15:50 +0200

    added buffer to cmd_sample

--- main.c
+++ main.c
@@ -38,17 +38,31 @@ static void cmd_sample (BaseSequentialStream *chp, int argc, char *argv[])
 {
     uint8_t data;
     BBI2C_t i2cdev;
+    uint8_t samples[10];
+    uint8_t samplecount = 0;
 
     DEBUG_INIT (chp);
 
     BBI2C_Init (&i2cdev, GPIOC, 10, GPIOC, 11, 50000, BBI2C_MODE_SLAVE);
 
+    //Store all captured Bytes in an array. Print after 10 captured bytes.
     chprintf (chp, "Sampling Line: ");
     for (;;)
     { 
         data = BBI2C_Get_Byte (&i2cdev);
-        chprintf (chp, "%x ", data);
-        chThdSleepMilliseconds(500);
+	if(samplecount < 10)
+	{
+        	samples[samplecount] = data;
+		samplecount++;
+	}
+	else
+	{
+		for(samplecount = 0; samplecount < 10; samplecount++)
+		{
+			chprintf(chp, "%x ", samples[samplecount]);
+		}
+		samplecount = 0;
+	}
     }
 }
 
